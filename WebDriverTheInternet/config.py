e9f716c6f2bae584fdaddc6081e7b4d5e6339e01
Dmitriy Malovaniy <malovaniy.dd@gmail.com>
2019-05-01 20:17:43 +0300

    try with 'platform' lib

--- WebDriverTheInternet/config.py
+++ WebDriverTheInternet/config.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import os
+import platform
 
 def path():
     dir_path = os.path.dirname(os.path.realpath(__file__))
 
-    if  "nt" in dir_path:
+    os_name = platform.system()
+    if  os_name == "Windows":
         CHROMEDRIVER = dir_path + '/chromedriver'
         return CHROMEDRIVER
     else:
         CHROMEDRIVER = "/usr/bin/chromedriver"
         return CHROMEDRIVER
+
 
 HEIGHT = 768
 WEIGHT = 1366
